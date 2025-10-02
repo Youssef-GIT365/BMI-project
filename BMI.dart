45f24e34004f046a824ae4bcbc26cd60c7ca4d62
Youssef-GIT365 <youssefahmed.bf@gmail.com>
2025-10-03 02:12:33 +0300

    Update BMI.dart

--- BMI.dart
+++ BMI.dart
@@ -3,21 +3,17 @@ import 'dart:io';
 void main() {
   print('=== BMI Calculator ===');
 
-  // Get weight from user
   stdout.write('Enter your weight (kg): ');
   double weight = double.parse(stdin.readLineSync()!);
 
-  // Get height from user
   stdout.write('Enter your height (cm): ');
   double heightCm = double.parse(stdin.readLineSync()!);
   double height = heightCm / 100;
 
-  // calculate BMI
   double bmi = weight / (height * height);
 
   print('Your BMI = ${bmi.toStringAsFixed(2)}');
 
-  // category part
   String category;
   if (bmi < 18.5) {
     category = 'Underweight';
